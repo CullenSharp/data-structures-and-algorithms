27576a4ff5bfbd4e694e9694c187819c56bcc794
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-14 19:01:23 -0700

    pass test 6

--- code-challenges/challenges-03.test.js
+++ code-challenges/challenges-03.test.js
@@ -58,6 +58,7 @@ Write a function named getBaseStatGreaterThan that, given the snorlaxData, below
 For example, getBaseStatGreaterThan(snorlaxData.stats, 50) will return an array containing the 'special-defense' and 'special-attack' objects.
 ------------------------------------------------------------------------------------------------ */
 
+//takes: snorlaxData.stats, 75
 const snorlaxData = {
   stats: [
     {
@@ -89,9 +90,7 @@ const snorlaxData = {
   weight: 4600,
 };
 
-const getBaseStatGreaterThan = (arr, minBaseStat) => {
-  // Solution code here...
-};
+const getBaseStatGreaterThan = (arr, minBaseStat) => arr.filter(value => value.baseStat > minBaseStat);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
@@ -238,7 +237,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return an array containing the stats that are greater than the input', () => {
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75)).toStrictEqual([ { stat: { url: 'https://pokeapi.co/api/v2/stat/5/', name: 'special-defense' }, effort: 2, baseStat: 110 } ]);
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75).length).toStrictEqual(1);

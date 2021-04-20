86bde233323df281b585a6d6aff91272ffba05a6
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-19 17:47:18 -0700

    Pass test 2

--- code-challenges/challenges-06.test.js
+++ code-challenges/challenges-06.test.js
@@ -33,9 +33,7 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 ------------------------------------------------------------------------------------------------ */
 
-const appendTheEnd = (str) => {
-  // Solution code here...
-};
+const appendTheEnd = (str) => str.slice()+" The end."
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
